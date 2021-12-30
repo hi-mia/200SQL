321cd6b16dcc5226546c669c86f5d8b899dd9650
idleline7 <idleline96@gmail.com>
2021-12-30 17:08:53 +0900

    21까지

--- part02_1.sql
+++ part02_1.sql
@@ -45,12 +45,46 @@ SELECT INSTR('abcdefg@naver.com', '@')
 
 SELECT SUBSTR('abcdefg@naver.com', INSTR('abcdefg@naver.com', '@')+1)
     FROM DUAL;
-
+ㅠ
 --naver만 출력
 SELECT RTRIM(SUBSTR('abcdefg@naver.com', INSTR('abcdefg@naver.com', '@')+1), '.com')
     FROM DUAL; --RTRIM: 오른쪽에 있는 .com을 잘라냄
 
 
 --20. 특정 철자를 다른 철자로 변경하기(REPLACE)
-SELECT ename, REPLACE(sal, 0, '*')
+SELECT ename, REPLACE(sal, 0, '*') -- 0을 *로 변환
     FROM emp;
+
+SELECT ename, REGEXP_REPLACE(sal, '[0-3]', '*') as SALARY -- 0~3까지를 *로 변환
+    FROM emp;    
+--REPLACE 함수: 특정 철자를 다른 철자로 변경하는 문자 함수
+
+--REGEXP_REPLACE 함수: 정규식 함수(더 복잡한 형태의 검색패턴으로 데이터 조회 가능)
+CREATE TABLE TEST_ENAME
+(ENAME VARCHAR2(10));
+
+INSERT INTO TEST_ENAME VALUES('김인호');
+INSERT INTO TEST_ENAME VALUES('안상수');
+INSERT INTO TEST_ENAME VALUES('최영희');
+COMMIT;
+
+--이름의 두 번째 자리의 한글을 *로 출력
+SELECT REPLACE(ENAME, SUBSTR(ENAME, 2, 1), '*') as "전광판_이름"
+    FROM test_ename;
+
+
+--21. 특정 철자를 N개 만큼 채우기(LPAD, RPAD)
+SELECT ename, LPAD(sal, 10, '*') as salary1, RPAD(sal, 10, '*') as salary2
+    FROM emp; --월급 컬럼 자릿수 10자리, 월급 출력하고 남은 나머지 자리 별표(*)
+
+SELECT ename, sal, lpad('■', round(sal/100), '■') as bar_chart
+    FROM emp;
+
+/*
+LPAD: 왼쪽(left)으로 채워 넣다(PAD)
+RPAD: 오른쪽으로 채워 넣음
+SQL로 데이터를 시각화하기에 유용
+*/   
+
+
+--22. 특정 절차 잘라내기 (TRIM, RTRIM, LTRIM)
